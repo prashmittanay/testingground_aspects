5780bf59468c1e6b146c53fc45ebcd1d0c5b62a7
Prashmit Tanay <prashmittanay@gmail.com>
2013-07-10 22:11:03 +0530

    Join Point in method arguments

--- src/main/java/testing/ground/model/Enhancers.java
+++ src/main/java/testing/ground/model/Enhancers.java
@@ -8,4 +8,14 @@ public class Enhancers {
 	public boolean validateURL(String url){
 		return GenericValidator.isUrl(url);
 	}
+	
+	public String removeProtocol(String url){
+		if(url.contains("http://")){
+			url = url.substring(6);
+		}else if(url.contains("https://")){
+			url = url.substring(7);
+		}
+		
+		return url;
+	}
 }
