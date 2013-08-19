0f7211b1fe37882d6de4bd515b8ba34068e5e8d6
Prashmit Tanay <tanay.wsd@nic.in>
2013-08-19 14:37:39 +0530

    report folder added. first jrxml added

--- src/main/java/testing/ground/rest/JasperRest.java
+++ src/main/java/testing/ground/rest/JasperRest.java
@@ -1,11 +1,16 @@
 package testing.ground.rest;
 
 import javax.sql.DataSource;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.sun.research.ws.wadl.Response;
 
 @Component
 @Path("/jasper")
@@ -13,5 +18,11 @@ public class JasperRest {
 	@Autowired DataSource dataSource;
 	@Autowired NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
-	
+	@GET
+	@Path("/compilefirstreport")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response compileFirstReport(){
+		
+		return null;
+	}
 }
