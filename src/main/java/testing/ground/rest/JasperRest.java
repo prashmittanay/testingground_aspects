2da82799b9f76c63f6c3025cd764113f186c9fba
Prashmit Tanay <tanay.wsd@nic.in>
2013-08-19 14:24:25 +0530

    Jasper rest added

---
+++ src/main/java/testing/ground/rest/JasperRest.java
@@ -0,0 +1,17 @@
+package testing.ground.rest;
+
+import javax.activation.DataSource;
+import javax.ws.rs.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/jasper")
+public class JasperRest {
+	@Autowired DataSource dataSource;
+	@Autowired NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	
+}
